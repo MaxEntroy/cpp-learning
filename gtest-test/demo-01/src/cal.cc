dcb1928a2a95b7f9eb6ca3a9427dd35904d9a168
MaxEntroy <likang@tju.edu.cn>
2020-01-16 18:10:26 +0800

    gtest-test

---
+++ gtest-test/demo-01/src/cal.cc
@@ -0,0 +1,16 @@
+#include "cal.h"
+
+#include <iostream>
+
+int add(int left, int right) {
+  if(left < 0 || right < 0) {
+    std::cerr << "Can only add positive numbers, received " << left << " and "
+      << right << std::endl;
+    return -1;
+  }
+
+  if(!left || !right)
+    return 0;
+
+  return left + right;
+}
