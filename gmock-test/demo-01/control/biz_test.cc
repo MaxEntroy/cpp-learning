16ff4e651813c695df6e9cd79092564d7cc0d81c
MaxEntroy <likang@tju.edu.cn>
2020-10-19 17:37:57 +0800

    gmock-test: Add demo-01.

--- gmock-test/demo-01/control/biz_test.cc
+++ gmock-test/demo-01/control/biz_test.cc
@@ -1,4 +1,95 @@
 #include "biz.h"
+
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
+using ::testing::Return;
+using ::testing::_;
 
+class MockUser : public gt::User {
+ public:
+  MOCK_METHOD2(Login, bool(const std::string&, const std::string&));
+  MOCK_METHOD1(Pay, bool(int));
+  MOCK_METHOD0(Online, bool());
+};
+
+class BizTest : public ::testing::Test {
+ protected:
+  gt::Biz foo;
+};
+
+TEST_F(BizTest, HandleInvlidUser) {
+  EXPECT_FALSE(foo.Pay("admin", "123", 100));
+}
+
+TEST_F(BizTest, HandleOfflineWithLoginFailed) {
+  MockUser user;
+  EXPECT_CALL(user, Online())
+    .Times(1)
+    .WillOnce(Return(false));
+  EXPECT_CALL(user, Login(_, _))
+    .Times(1)
+    .WillOnce(Return(false));
+
+  foo.SetUser(&user);
+  EXPECT_FALSE(foo.Pay("admin", "123", 100));
+}
+
+TEST_F(BizTest, HandleOfflineWithLoginSuccessfullyAndPayFailed) {
+  MockUser user;
+  EXPECT_CALL(user, Online())
+    .Times(1)
+    .WillOnce(Return(false));
+  EXPECT_CALL(user, Login(_, _))
+    .Times(1)
+    .WillOnce(Return(true));
+  EXPECT_CALL(user, Pay(_))
+    .Times(1)
+    .WillOnce(Return(false));
+
+  foo.SetUser(&user);
+  EXPECT_FALSE(foo.Pay("admin", "123", 100));
+}
+
+TEST_F(BizTest, HandleOfflineWithLoginSuccessfullyAndPaySuccessfully) {
+  MockUser user;
+  EXPECT_CALL(user, Online())
+    .Times(1)
+    .WillOnce(Return(false));
+  EXPECT_CALL(user, Login(_, _))
+    .Times(1)
+    .WillOnce(Return(true));
+  EXPECT_CALL(user, Pay(_))
+    .Times(1)
+    .WillOnce(Return(true));
+
+  foo.SetUser(&user);
+  EXPECT_TRUE(foo.Pay("admin", "123", 100));
+}
+
+TEST_F(BizTest, HandleOnlineWithPaySuccessfully) {
+  MockUser user;
+  EXPECT_CALL(user, Online())
+    .Times(1)
+    .WillOnce(Return(true));
+  EXPECT_CALL(user, Pay(_))
+    .Times(1)
+    .WillOnce(Return(true));
+
+  foo.SetUser(&user);
+  EXPECT_TRUE(foo.Pay("admin", "123", 100));
+}
+
+TEST_F(BizTest, HandleOnlineWithPayFailed) {
+  MockUser user;
+  EXPECT_CALL(user, Online())
+    .Times(1)
+    .WillOnce(Return(true));
+  EXPECT_CALL(user, Pay(_))
+    .Times(1)
+    .WillOnce(Return(false));
+
+  foo.SetUser(&user);
+  EXPECT_FALSE(foo.Pay("admin", "123", 100));
+
+}
--- gmock-test/demo-01/role/user.h
+++ gmock-test/demo-01/role/user.h
@@ -3,6 +3,8 @@
 
 #include <string>
 
+namespace gt {
+
 class User {
  public:
   User() = default;
@@ -10,11 +12,13 @@ class User {
   User(const User&) = delete;
   User& operator=(const User&) = delete;
 
-  virtual ~User();
+  virtual ~User() = default;
 
   virtual bool Login(const std::string& user_name, const std::string& pwd) = 0;
   virtual bool Pay(int money) = 0;
   virtual bool Online() = 0;
-};
+};  // class User
 
-#endif
+}  // namespace gt
+
+#endif  // namespace USER_H_
