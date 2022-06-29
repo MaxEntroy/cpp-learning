fbda885124bc1c2c9ea6ae7d1ef457420e7f2227
MaxEntroy <likang@tju.edu.cn>
2022-06-29 11:57:48 +0800

    template-test: demo-03 is add.

--- template-test/demo-03/main.cc
+++ template-test/demo-03/main.cc
@@ -16,7 +16,7 @@ std::ostream& print(std::ostream& os, const T& t) {
 
 template<typename T, typename... Args>
 std::ostream& print(std::ostream& os, const T& t, const Args&... rest) {
-  os << t << ",";
+  os << t << "|";
   return print(os, rest...);
 }
 
@@ -25,12 +25,47 @@ std::ostream& err_msg(std::ostream& os, const Args&... rest) {
   return print(os, debug_rep(rest)...);
 }
 
+namespace pf {
+
+void f(int&& v1, int& v2) {
+  std::cout << "f(int&&, int&) is called, v1 = " << ++v1 << ", v2 = " << v2++ << std::endl;
+}
+
+void g(int& v1, int&& v2) {
+  std::cout << "g(int&, int&&) is called, v1 = " << v1++ << ", v2 = " << ++v2 << std::endl;
+}
+
+void h(std::string& s1, std::string&& s2, char& split) {
+  std::cout << "h(string&, string&&, char&) is called, " << s1 << split << s2 << std::endl;
+}
+
+template<typename Func, typename... Args>
+void wrapper(Func func, Args&&... args) {
+  func(std::forward<Args>(args)...);
+}
+
+void test() {
+  int val = 3;
+  int& r{val};
+  wrapper(f, 5, r);
+  wrapper(g, r, 5);
+
+  std::string s{"perfect"};
+  std::string& rs{s};
+  char split{','};
+  char& rc{split};
+  wrapper(h, rs, "forwarding", rc);
+}
+
+}  // namespace pf
+
+
 void test() {
   //print(std::cout, "We will print a lot of vars of difference type" , 32, 3.14, "and a string: hello,world.");
   err_msg(std::cout, "We will print a lot of vars of difference type" , 32, 3.14, "and a string: hello,world.");
 }
 
 int main(void) {
-  test();
+  pf::test();
   return 0;
 }
